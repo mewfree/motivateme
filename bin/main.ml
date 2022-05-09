fc28b002cd90814ab97b861f8959a0754ad0c581
Damien Gonot <damien.gonot@gmail.com>
2022-05-08 20:01:38 -0400

    run ocamlformat

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,10 @@
-let quotes = [
-  "Create bite-sized tasks for your project";
-  "Remove distractions (close browser tabs, put your phone away)";
-  "Take a small break, drink from water and come back freshen up!";
-  "Focus on the big picture, remember why you're doing this"
-]
+let quotes =
+  [
+    "Create bite-sized tasks for your project";
+    "Remove distractions (close browser tabs, put your phone away)";
+    "Take a small break, drink from water and come back freshen up!";
+    "Focus on the big picture, remember why you're doing this";
+  ]
 
 let _ = Random.self_init ()
 let () = print_endline (List.nth quotes (Random.int (List.length quotes)))
