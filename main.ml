5d585f331d20ca327d00e0c93c8fe8a66003e596
Damien Gonot <damien.gonot@gmail.com>
2022-12-16 10:04:31 -0500

    consistent syntax

--- main.ml
+++ main.ml
@@ -1,10 +1,10 @@
 let quotes =
   [
-    "Create bite-sized tasks for your project";
-    "Remove distractions (close browser tabs, put your phone away)";
+    "Create bite-sized tasks for your project.";
+    "Remove distractions (close browser tabs, put your phone away).";
     "Take a small break, drink from water and come back freshen up!";
-    "Focus on the big picture, remember why you're doing this";
-    "You will never regret good work once it is done"
+    "Focus on the big picture, remember why you're doing this.";
+    "You will never regret good work once it is done."
   ]
 
 let _ = Random.self_init ()
