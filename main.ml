5831c1781417f76bfe3af31139b60bed81382013
Damien Gonot <damien.gonot@gmail.com>
2022-12-16 10:03:15 -0500

    add quote good work done

--- main.ml
+++ main.ml
@@ -4,6 +4,7 @@ let quotes =
     "Remove distractions (close browser tabs, put your phone away)";
     "Take a small break, drink from water and come back freshen up!";
     "Focus on the big picture, remember why you're doing this";
+    "You will never regret good work once it is done"
   ]
 
 let _ = Random.self_init ()
